5ce73b9b4577f6db75a3f04a36500925e4dc84bb
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2022-12-12 18:36:40 +0100

    backend call steam api

---
+++ src/app.service.ts
@@ -0,0 +1,106 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+import {
+  getGameDetailsURL,
+  getGameReviewsURL,
+  getGameTopURL,
+  searchGameURL,
+} from './url';
+
+@Injectable()
+export class AppService {
+  async getTop(size = 10): Promise<any[]> {
+    const response = await axios.get(getGameTopURL());
+
+    if (response.status === 200 && response) {
+      let formatedResponse = [];
+      const ranks = response.data.response.ranks;
+
+      formatedResponse = ranks.slice(0, size);
+      formatedResponse = await Promise.all(
+        formatedResponse.map(async (value) => {
+          const gameResponse = await axios.get(getGameDetailsURL(value.appid));
+
+          if (gameResponse.status === 200) {
+            return {
+              id: value.appid,
+              name: gameResponse.data[value.appid].data.name,
+              editor: gameResponse.data[value.appid].data.developers[0],
+              cover_image: gameResponse.data[value.appid].data.header_image,
+              background_image: gameResponse.data[value.appid].data.background,
+              price:
+                gameResponse.data[value.appid].data.price_overview
+                  ?.final_formatted ?? 'Gratuit',
+            };
+          }
+
+          return { appid: value.appid };
+        }),
+      );
+
+      return Promise.resolve(formatedResponse);
+    }
+
+    return Promise.reject([]);
+  }
+
+  async getDetails(id: string): Promise<any> {
+    const response = await axios.get(getGameDetailsURL(id));
+    const responseReviews = await axios.get(getGameReviewsURL(id));
+
+    const formatedReviews = await Promise.all(
+      responseReviews.data.reviews.map((r) => {
+        return {
+          name: 'Patate',
+          good_grade: r.voted_up,
+          review: r.review,
+        };
+      }),
+    );
+
+    if (response.status === 200) {
+      return Promise.resolve({
+        id,
+        name: response.data[id].data.name,
+        editor: response.data[id].data.developers[0],
+        cover_image: response.data[id].data.header_image,
+        background_image: response.data[id].data.background,
+        price:
+          response.data[id].data.price_overview?.final_formatted ?? 'Gratuit',
+        description: response.data[id].data.detailed_description,
+        reviews: formatedReviews,
+      });
+    }
+
+    return Promise.reject({});
+  }
+
+  async searchGame(text: string): Promise<any> {
+    const response = await axios.get(searchGameURL(text));
+
+    if (response.status === 200) {
+      const formatedResponse = await Promise.all(
+        response.data.items.map(async (item) => {
+          const detailsResponse = await axios.get(getGameDetailsURL(item.id));
+
+          return {
+            id: item.id,
+            name: item.name,
+            editor: detailsResponse.data[item.id].data.developers[0],
+            cover_image: detailsResponse.data[item.id].data.header_image,
+            background_image: detailsResponse.data[item.id].data.background,
+            price:
+              detailsResponse.data[item.id].data.price_overview
+                ?.final_formatted ?? 'Gratuit',
+          };
+        }),
+      );
+      return Promise.resolve({
+        count: response.data.total,
+        result: formatedResponse,
+      });
+    }
+    return Promise.reject({ count: 0 });
+  }
+}
